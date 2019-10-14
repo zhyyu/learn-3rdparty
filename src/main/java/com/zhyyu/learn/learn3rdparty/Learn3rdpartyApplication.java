041fee71777c4ffb5c57087661c9ab8bdb6f7cd9
juror.yu <juror.yu@mart.blue>
2019-10-14 14:36:58 +0800

    init

---
+++ src/main/java/com/zhyyu/learn/learn3rdparty/Learn3rdpartyApplication.java
@@ -0,0 +1,13 @@
+package com.zhyyu.learn.learn3rdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learn3rdpartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Learn3rdpartyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhyyu/learn/learn3rdparty/Learn3rdpartyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhyyu.learn.learn3rdparty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Learn3rdpartyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
