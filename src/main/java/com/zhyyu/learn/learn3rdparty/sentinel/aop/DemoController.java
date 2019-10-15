e867d568ffc5889ee84af35a0bfcad789d0e83d0
juror.yu <juror.yu@mart.blue>
2019-10-15 10:28:47 +0800

    sentinel aop test

---
+++ src/main/java/com/zhyyu/learn/learn3rdparty/sentinel/aop/DemoController.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zhyyu.learn.learn3rdparty.sentinel.aop;
+
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Eric Zhao
+ */
+@RestController
+public class DemoController {
+
+    @Autowired
+    private TestService service;
+
+    /*@PostConstruct
+    private void initRules() {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("hello");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(1);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }*/
+
+    @GetMapping("/foo")
+    public String apiFoo(@RequestParam(required = false) Long t) throws Exception {
+        if (t == null) {
+            t = System.currentTimeMillis();
+        }
+        service.test();
+        return service.hello(t);
+    }
+
+    @GetMapping("/baz/{name}")
+    public String apiBaz(@PathVariable("name") String name) {
+        return service.helloAnother(name);
+    }
+
+
+}
